078c148146aa93449abac86e69ff7e958c198d83
Anju Sebastian <christeenaanju@gmail.com>
2024-08-27 23:15:43 +0530

    update

--- src/Components/Seller/SellerSignup.jsx
+++ src/Components/Seller/SellerSignup.jsx
@@ -5,6 +5,8 @@ import * as yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import instance from "../../axios";
+
 
 const schema = yup
   .object({
