6a190da65f7940c8eb055a441f3882a3e094d30f
Anju Sebastian <christeenaanju@gmail.com>
2024-09-19 20:32:45 +0530

    added S

--- src/Components/Seller/SellerSignup.jsx
+++ src/Components/Seller/SellerSignup.jsx
@@ -83,7 +83,7 @@ const SellerSignup = () => {
       {errors.password && <p>{errors.password.message}</p>}
       <input type="submit" className="rounded-md bg-green-500 py-1 text-white" />
       <p>
-        Seller already exist{" "}
+        seller already exist{" "}
         <Link to="/seller/login" className="text-green-500 underline">
           Signin
         </Link>
